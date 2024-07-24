86926db295f9e027dc2c1ae409ca422dbbcd55a9
ohm0106 <134082781+ohm0106@users.noreply.github.com>
2024-07-24 12:02:23 +0900

    feat: backbutton매니저 추가

--- Assets/Scripts/Util/BackButtonManager.cs
+++ Assets/Scripts/Util/BackButtonManager.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class BackButtonManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] List<GameObject> panelsToClose;  // List of panels to close
+    [SerializeField] GameObject lobbyPanel;
+
+    private static Stack<GameObject> panelHistory = new Stack<GameObject>();
+
+    void Awake()
     {
-        
+        panelHistory.Push(lobbyPanel); // init
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBackButtonClicked()
     {
-        
+        if (panelHistory.Count > 0)
+        {
+            GameObject lastOpenedPanel = panelHistory.Pop();
+            lastOpenedPanel.SetActive(false);
+        }
+        else
+        {
+            //todo : App Close
+            Debugger.PrintLog("BackButtonManager: No panels to close.",LogType.Warning);
+        }
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        if (panelsToClose.Contains(panel))
+        {
+            panelHistory.Push(panel);
+            panel.SetActive(true);
+        }
+        else
+        {
+            Debugger.PrintLog("BackButtonManager: Panel is not in the list of panels to close.",LogType.Warning);
+        }
     }
 }
